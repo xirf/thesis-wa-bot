205de95d003c83e70007d309525c835be66719ed
xirf <andikanp@proton.me>
2024-09-03 09:49:51 +0700

    feat: modularize the command

--- src/command/student/collectReport.ts
+++ src/command/student/collectReport.ts
@@ -6,44 +6,66 @@ import database from "../../database";
 import { Message } from "whatsapp-web.js";
 import client from "../../lib/waweb";
 
+const handleCancel = async (msg: Message, cache: any) => {
+    msg.reply(response.canceled);
+    cache.set(msg.from, { data: cache.data });
+};
+
+
+const handleFinish = async (msg: Message, cache: any) => {
+    if (cache.msgs == undefined || cache.msgs.length == 0) {
+        return await msg.reply(response.error.emptyReport);
+    }
+    return await sendReport(msg, cache.msgs, cache.get(msg.from), "student");
+};
+
+
+const handleDelete = async (msg: Message, cache: any) => {
+    if (cache.msgs == undefined || cache.msgs.length == 0) {
+        return await msg.reply(response.error.emptyReport);
+    }
+
+    let newMessage = cache.msgs.filter((message: any) => message != msg.mentionedIds[ 0 ]);
+    await msg.react("🚫");
+    cache.set(msg.from, newMessage);
+
+    let savedMessage = await database.chat.findFirst({
+        where: { id: msg.id.id }
+    });
+
+    if (savedMessage) {
+        try {
+            client.sendMessage(msg.from, response.messageCanceled);
+        } catch (error) {
+            logger.error({ error }, "Failed to send reply message");
+        }
+    }
+};
+
+
+const handleDefault = async (msg: Message, cache: any) => {
+    await msg.react("📝");
+    if (cache.msgs == undefined) cache.msgs = [];
+    cache.msgs.push(msg.id.id);
+};
+
 const command: Command = async (msg: Message, cache: any) => {
     try {
-        let text: string | null = msg.body;
+        let text: string | null = msg.body?.toLowerCase();
 
-        if (text?.toLowerCase() == "batal") {
-            msg.reply(response.canceled);
-            cache.set(msg.from, { data: cache.data })
-            return;
-        } else if (text?.toLowerCase() == "selesai") {
-            if (cache.msgs == undefined || cache.msgs.length == 0)
-                return await msg.reply(response.error.emptyReport);
-
-            return await sendReport(msg, cache.msgs, cache.get(msg.from), "student");
-        } else if (text?.toLowerCase() == "hapus") {
-            if (cache.msgs == undefined || cache.msgs.length == 0)
-                return await msg.reply(response.error.emptyReport);
-
-            cache.msgs = cache.msgs.filter((message: any) => message != msg.mentionedIds[ 0 ]);
-            await msg.react("🚫")
-
-            let savedMessage = await database.chat.findFirst({
-                where: {
-                    id: msg.id.id
-                }
-            })
-
-            if (savedMessage) {
-                try {
-                    client.sendMessage(msg.from, response.messageCanceled)
-                } catch (error) {
-                    logger.error({error}, "Failed to send reply message")
-                }
-            }
-        } else {
-            await msg.react("📝");
-
-            if (cache.msgs == undefined) cache.msgs = [];
-            cache.msgs.push(msg.id.id);
+        switch (text) {
+            case "batal":
+                await handleCancel(msg, cache);
+                break;
+            case "selesai":
+                await handleFinish(msg, cache);
+                break;
+            case "hapus":
+                await handleDelete(msg, cache);
+                break;
+            default:
+                await handleDefault(msg, cache);
+                break;
         }
 
         return await cache.set(msg.from, cache.get(msg.from));
@@ -58,6 +80,6 @@ const command: Command = async (msg: Message, cache: any) => {
         });
         return await msg.reply(response.error.internalServerError);
     }
-}
+};
 
 export default command;
--- src/utils/separateMediaAndText.ts
+++ src/utils/separateMediaAndText.ts
@@ -1,5 +1,5 @@
 export default function separateMediaAndTextReports(allReports: any[]) {
-    const media = [];
+    const media = allReports.filter(({ type }) => [ "video", "image", "document" ].includes(type));
     const report = allReports.filter(({ type }) => type === "chat");
     const reportText = report.map(({ content }) => content).join("\n");
     return { media, reportText };
